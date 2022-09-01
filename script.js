1926e1491da6c08c41d532e4f75f03ce8ac85a28
HamdiHasbullah <sansedyy@gmail.com>
2022-09-01 20:15:53 +0800

    modified form submit function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function notifications() {
-    alert("Your message has been sent")
+function validateForm() {
+    let x = document.forms["form1"]["name"].value;
+    if (x == "") {
+        alert("please fill the information")
+    }
+    else {
+        alert("message send")
+    }
+    return false;
 }
 
 
