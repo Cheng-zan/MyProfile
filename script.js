17a4911c524567af319e3731babab49b90d32ca7
HamdiHasbullah <sansedyy@gmail.com>
2022-09-02 17:00:38 +0800

    try n error on javascript validatForm() function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function validateForm() {
     let x = document.forms["form1"]["name"].value;
     if (x == "") {
-        alert("please fill the information")
+        alert("please fill the information")    
+        return false;
     }
     else {
         alert("message send")
+        return true;
     }
-    return false;
+
 }
 
 
