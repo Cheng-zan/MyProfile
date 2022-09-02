d150f7ae4fc60dc3822c3cf387502df193eae274
HamdiHasbullah <sansedyy@gmail.com>
2022-09-02 20:47:34 +0800

    add an image and change the background image in contact.html

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function validateForm() {
     let x = document.forms["form1"]["name"].value;
     if (x == "") {
-        alert("please fill the information")    
+        alert("Please fill the information")    
         return false;
     }
     else {
         alert("message send")
         return true;
     }
-
 }
 
 
